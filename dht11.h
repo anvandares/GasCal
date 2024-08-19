60adcc528d41bb1b0dd2be236a69f9d717c717b7
anvandares <liv.danielsdottir@gmail.com>
2024-08-19 14:41:21 +0200

    ändrat funktionsnamnen så att det är konsekvent

--- dht11.h
+++ dht11.h
@@ -1,16 +1,15 @@
 #include <DHT.h>
-#define DHTPIN 2
-#define DHTTYPE DHT11
+#define DHTPIN 2 //The pin that the sensor is connected to
+#define DHTTYPE DHT11 //Defines DHT type
 DHT dht(DHTPIN, DHTTYPE);
 
-int dhtTemp()
-{
-    float t = dht.readTemperature();
+int dht_temp(){
+    float t = dht.readTemperature(); //Reads the temperature in celsius
     int tempInt=int(t);
     return tempInt;
 
 }
-int dhtHum(){
+int dht_hum(){
 float h = dht.readHumidity();
 int humInt=int(h);
 return humInt;
