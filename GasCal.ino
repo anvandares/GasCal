6698413f4b1487ed379ad79fbc06c045312214a5
anvandares <liv.danielsdottir@gmail.com>
2024-08-19 13:38:40 +0200

    flyttat runt lite funtkioner

--- GasCal.ino
+++ GasCal.ino
@@ -5,9 +5,8 @@
 #include "WiFi.h"
 
 #include <Wire.h>
-unsigned long timeStamp = millis(); //Notes the time when program started. 
 unsigned long lastConnectionTime = 0;  // last time you connected to the server, in milliseconds
-String servStr= server.toString(); //To add IP-adress to sprintf and print to client 
+
 
 void setup() {
 /* -------------------------------------------------------------------------- */  
@@ -45,8 +44,7 @@ void loop() {
   read_request(); 
   
   if(_delay(lastConnectionTime,10000UL)){  // if ten seconds have passed since your last connection,
-      httpRequest();                       // then connect again and send data:
-    
+      httpRequest(client_print());                       // then connect again and send data:  
       lastConnectionTime = millis();   // notes the time that the connection was made:
         
 
@@ -57,7 +55,8 @@ String client_print()
 {
   
   int gas=detectGas();
-  getTempHum();
+  int temp=get_temp();
+  int hum=get_hum();
   String time=get_time();
   char reportTemp[1020];
  
@@ -71,42 +70,6 @@ String client_print()
 
 }
 
- 
-// this method makes a HTTP connection to the server:
-/* -------------------------------------------------------------------------- */
-void httpRequest() {
-/* -------------------------------------------------------------------------- */  
-  // close any connection before send a new request.
-  // This will free the socket on the NINA module
-  client.stop();
-  char buffer[100]; //For printing host 
-
-  // if there's a successful connection:
-  if (client.connect(server, port)) {
-    
-    Serial.println("\nconnecting...");
-    
-    // send the HTTP GET request:
-    client.println(client_print());
-    client.println("GET / HTTP/1.1");
-    
-    sprintf(buffer, "Host: %s:/%d HTTP/1.1\n", servStr, port); 
-    client.print(buffer); //PRINTS HOST  
-   
-    client.println("User-Agent: ArduinoWiFi/1.1");
-    client.println("Connection: close");
-    
-    
-    client.println();
-
-
-   
-  } else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-   
-  }
-}
 
 
 
