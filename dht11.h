7313b57c8ca804285331a4778f9f29b09a46ce3e
anvandares <liv.danielsdottir@gmail.com>
2024-08-19 15:18:19 +0200

    first commit"ändrat namn på ino filen, råkade strula till det när jag klonade, ändrat lite i dht-headern

--- dht11.h
+++ dht11.h
@@ -3,14 +3,16 @@
 #define DHTTYPE DHT11 //Defines DHT type
 DHT dht(DHTPIN, DHTTYPE);
 
-int dht_temp(){
+int dht_temp(){ //Reads temperature in float and returns it in int
+
     float t = dht.readTemperature(); //Reads the temperature in celsius
     int tempInt=int(t);
     return tempInt;
 
 }
-int dht_hum(){
-float h = dht.readHumidity();
-int humInt=int(h);
-return humInt;
+int dht_hum(){//Reads humidity in float and returns it in int
+   
+  float h = dht.readHumidity();
+  int humInt=int(h);
+  return humInt;
 }
